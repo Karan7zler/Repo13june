3aa9ed26c746317ed28700467dfd417c907d1b88
Karan7zler <23karan2gmail.com>
2023-06-13 11:42:19 +0530

    june 13 codes

---
+++ src/org/test13/June13New.java
@@ -0,0 +1,31 @@
+package org.test13;
+
+public class June13New {
+	
+	private void method1() {
+		
+		System.out.println("Method one sample");
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		June13New j = new June13New();
+		
+		j.method1();
+		
+	}
+
+}
